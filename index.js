c0ab91cf3f6eb2e6dd75c5e018093a116b27f895
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-21 11:24:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+
+let num3 = 44;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,20);
